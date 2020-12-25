3a6a8bbb36450ec39aaf14c842aeddeaf5eac534
gavfu <gavfu@outlook.com>
2020-12-25 13:40:20 +0000

    Automated release: 2020-12-25, 13:40:20

--- docs/service-worker.js
+++ docs/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "./precache-manifest.a7455e1d5b329a3040f4ab1cc6b75e65.js"
+  "./precache-manifest.f4a9dbb0080a98a54042d14656afaf9f.js"
 );
 
 self.addEventListener('message', (event) => {
